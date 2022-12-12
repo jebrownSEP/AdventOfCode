34c022d8a0d53c9971bbba4d9401076cb25a344b
Joe Brown <jebrown@sep.com>
2022-12-12 13:06:53 -0500

    Day 11 part 2

--- day11/day11.ts
+++ day11/day11.ts
@@ -6,8 +6,8 @@ import { join } from 'path';
 function getMonkeysAttributesAfter20Rounds() {
     try {
         const lines = getFileByLinesSync('./day11/day11.txt');
-        const part1 =  getMonkeysAfter20Rounds(lines);
-        const part2: number[] = [];
+        const part1 =  getMonkeysAfterXRoundsAndDivideBy3(lines, 20, true);
+        const part2 = getMonkeysAfterXRoundsAndDivideBy3(lines, 10000, false);
         return {part1, part2}
       } catch (err) {
         console.error(err);
@@ -16,19 +16,23 @@ function getMonkeysAttributesAfter20Rounds() {
 }
 
 interface Monkey {
-  currentItems: number[];
-  performOperation(item: number): number;
-  throwItem(item: number): void;
+  currentItems: bigint[];
+  performOperation(item: bigint): bigint;
+  throwItem(item: bigint): void;
   numberInspectedItems: number;
+  divisibleBy: bigint;
 }
 
-function getMonkeysAfter20Rounds(lines: string[]) {
-  const monkeys: Monkey[] = getStartingMonkeys(lines);
+function getMonkeysAfterXRoundsAndDivideBy3(lines: string[], numberOfRounds: number, divideByThree: boolean) {
+  const monkeys: Monkey[] = getStartingMonkeys(lines, divideByThree);
+  const LCM = monkeys.map((monkey) => monkey.divisibleBy).reduce((prev, current) => prev * current );
+  // console.log('LCM', LCM);
 
-  for(let i = 0; i < 20; i++) {
+  for(let i = 0; i < numberOfRounds; i++) {
+    // console.log('round ', i+ 1);
     for(let j = 0; j < monkeys.length; j++) {
       for(let k = 0; k < monkeys[j].currentItems.length; k++) {
-        const updatedItem =  monkeys[j].performOperation(monkeys[j].currentItems[k]);
+        const updatedItem =  monkeys[j].performOperation(monkeys[j].currentItems[k]) % LCM;
         monkeys[j].throwItem(updatedItem);
         monkeys[j].numberInspectedItems += 1;
       }
@@ -39,35 +43,38 @@ function getMonkeysAfter20Rounds(lines: string[]) {
   return monkeys;
 }
 
-function getStartingMonkeys(lines: string[]) {
+function getStartingMonkeys(lines: string[], divideByThree: boolean) {
   const monkeys: Monkey[] = [];
   let index = 0;
 
   while(index < lines.length) {
     const currentItemsLine = lines[index + 1];
-    const currentItems = currentItemsLine.split('Starting items: ')[1].split(', ').map((item) => parseInt(item));
+    const currentItems = currentItemsLine.split('Starting items: ')[1].split(', ').map((item) => BigInt(item));
 
     const operationLine = lines[index + 2];
     const [operatorString, operandString] = operationLine.split('Operation: new = old ')[1].split(' ');
     
     let performOperation;
     if(operatorString === '+') {
-      performOperation = (item: number) => item + parseInt(operandString);
+      performOperation = (item: bigint) => item + BigInt(operandString);
     } else { // '*'
       if(operandString === 'old') {
-        performOperation = (item: number) => item * item;
+        performOperation = (item: bigint) => item * item;
       } else {
-        performOperation = (item: number) => item * parseInt(operandString);
+        performOperation = (item: bigint) => item * BigInt(operandString);
       }
     }
     const throwItemTestLine = lines[index + 3];
-    const divisibleBy = parseInt(throwItemTestLine.split('Test: divisible by ')[1]);
+    const divisibleBy = BigInt(throwItemTestLine.split('Test: divisible by ')[1]);
     const monkeyNumberTrue = parseInt(lines[index + 4].split('If true: throw to monkey ')[1]);
     const monkeyNumberFalse = parseInt(lines[index + 5].split('If false: throw to monkey ')[1]);
 
-    const throwItem = (item: number) => {
-      const updatedItem = Math.floor(item / 3);
-      if(updatedItem % divisibleBy === 0) {
+    const throwItem = (item: bigint) => {
+      let updatedItem = item;
+      if(divideByThree) {
+        updatedItem = item / 3n;
+      }
+      if(updatedItem % divisibleBy === 0n) {
         monkeys[monkeyNumberTrue].currentItems.push(updatedItem);
       } else {
         monkeys[monkeyNumberFalse].currentItems.push(updatedItem);
@@ -77,7 +84,8 @@ function getStartingMonkeys(lines: string[]) {
       currentItems,
       performOperation,
       throwItem,
-      numberInspectedItems: 0
+      numberInspectedItems: 0,
+      divisibleBy,
     });
 
     index += 7;
@@ -95,9 +103,11 @@ console.log(end - start);
 console.log(part1);
 console.log("Part 1");
 const sortedMonkeys = part1.sort((a, b) => b.numberInspectedItems -  a.numberInspectedItems);
-const moknkeyBusiness = sortedMonkeys[0].numberInspectedItems * sortedMonkeys[1].numberInspectedItems;
-console.log(moknkeyBusiness);
+const monkeyBusiness = sortedMonkeys[0].numberInspectedItems * sortedMonkeys[1].numberInspectedItems;
+console.log(monkeyBusiness);
 
-// console.log("Part 2");
-// console.log(part2);
-
+console.log(part2);
+console.log("Part 2");
+const sortedMonkeys2 = part2.sort((a, b) => b.numberInspectedItems -  a.numberInspectedItems);
+const monkeyBusiness2 = sortedMonkeys2[0].numberInspectedItems * sortedMonkeys2[1].numberInspectedItems;
+console.log(monkeyBusiness2);
